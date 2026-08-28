6337d263a6f8f094256bb13dc08f8a2bcd8312b0
Mizuho Uehara <mu@dhcp-9-73-203-59.4s6-jp.ibm.com>
2026-08-28 17:10:43 +0900

    Update onboarding-docs

--- onboarding-docs/_nuxt/BRXgLEbc.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./D86PTcB3.js";import{r as e,c as i}from"./BSxVasRs.js";const[n]=o("ConfigProvider");function s(r){const t=n({dir:e("ltr")});return i(()=>r?.value||t.dir?.value||"ltr")}export{n as i,s as u};
---
+++ onboarding-docs/_nuxt/C0sHPBaQ.js
@@ -0,0 +1,1 @@
+import{c as o}from"./BCIEFrS5.js";import{r as e,c as i}from"./CfuboEk_.js";const[n]=o("ConfigProvider");function s(r){const t=n({dir:e("ltr")});return i(()=>r?.value||t.dir?.value||"ltr")}export{n as i,s as u};
